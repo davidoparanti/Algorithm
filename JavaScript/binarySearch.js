30d05b94a2e44310bb4ae00011e4ea30ddd043ed
David Oparanti <david.oparanti@gmail.com>
2020-07-17 00:09:32 +0100

    Implement binary Search

--- JavaScript/binarySearch.js
+++ JavaScript/binarySearch.js
@@ -1,3 +1,17 @@
 function binarySearch(arr, val) {
-    
+    let min = 0; 
+    let max = array.length - 1;
+     
+    while (man <= max) {
+        let middle = Math.floor((min + max) / 2);
+        
+        if(arr[middle] < val) {
+            min = middle + 1;
+        } else if(arr[middle] > val) {
+            max = middle - 1
+        } else {
+            return middle
+        }
+    }
+    return - 1;
 }
