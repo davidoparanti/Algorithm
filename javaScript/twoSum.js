2b4b38ba4f8052ce7e9b68145164364a585d5f3e
David Oparanti <david.oparanti@gmail.com>
2020-07-16 10:18:05 +0100

    implement Two sum  in js

---
+++ javaScript/twoSum.js
@@ -0,0 +1,18 @@
+function twoSum(array) {
+    let left = 0;
+    let rigth = array.length -1;
+    let rigth = array.length - 1;
+
+    while(left < right) {
+        let sum = array[left] + array[rigth];
+        if(sum === 0) {
+            return [array[left], array[rigth]];
+        } else if (sum > 0) {
+            rigth++
+        } else {
+            left++
+        }
+        
+    }
+    return null; 
+}
