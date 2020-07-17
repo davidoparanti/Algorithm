3544fb70c3aca872b03ae6ea19443631cc17bb44
David Oparanti <david.oparanti@gmail.com>
2020-07-17 12:03:56 +0100

    Add push method

--- JavaScript/singlyLinkedList.js
+++ JavaScript/singlyLinkedList.js
@@ -19,5 +19,16 @@ class SinglyLinkedList {
         
     }
 
-    
+    push(val) {
+        let newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode 
+            this.tail = newNode; 
+        }
+        this.length++
+        return this;
+    }
 }
