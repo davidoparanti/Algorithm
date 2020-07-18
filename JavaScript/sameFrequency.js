10e657466363c2047a87beffd2896c595f868be9
David Oparanti <david.oparanti@gmail.com>
2020-07-18 14:55:39 +0100

    bug fix

--- JavaScript/sameFrequency.js
+++ JavaScript/sameFrequency.js
@@ -37,25 +37,3 @@
         return true;     
         
   }
-
-  function search(array, val) {
-   let min = 0;
-   let max = array.length - 1;
-  
-  while(min <= max) {
-    let middle = Math.floor((min + max) / 2 );
-    let currentElement = array[middle];
-    
-    if(array[middle] < val) {
-      min = middle + 1;
-    } else if(array[middle] > val) {
-      max = middle - 1;
-    } else {
-      return middle;
-    }
-  }
-    return - 1;
-    
-}
-  
- search([1,2,3,4,5,6,7,8], 9)
--- javaScript/twoSum.js
+++ javaScript/twoSum.js
@@ -1,22 +1,20 @@
 function twoSum(array) {
     let left = 0;
-    let rigth = array.length -1;
-  
-    while(left < right) {
-        let sum = array[left] + array[rigth];
-        if(sum === 0) {
-            return [array[left], array[rigth]];
+    let right = array.length - 1;
+
+    while (left < right) {
+        let sum = array[left] + array[right];
+        if (sum === 0) {
+            return [array[left], array[right]];
         } else if (sum > 0) {
-            rigth++
+            right--
         } else {
             left++
         }
-        
-    }
-    return null; 
-}
 
-twoSum([-2,-1, 0,2,3])
+    }
+    //return null; 
+}
 
 
 
