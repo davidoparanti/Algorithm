2e3cb7d09796e31c1cc98b3f2f84e1a33066fce2
David Oparanti <david.oparanti@gmail.com>
2020-07-19 18:49:06 +0100

    Add less than

--- Java/Tictactoo.java
+++ Java/Tictactoo.java
@@ -1,26 +1,38 @@
-//package tictactoe;
-import java.util.*; 
-public class Tictactoo {
-    public static void main(String[] args) {  // oxx oxo xox
-        // write your code here
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        System.out.println("---------");
+// //package tictactoe;
+// import java.util.*; 
+// public class Tictactoo {
+//     public static void main(String[] args) {  // oxx oxo xox
+//         // write your code here
+//         Scanner sc = new Scanner(System.in);
+//         String input = sc.nextLine();
+//         System.out.println("---------");
 
-        for(int i = 0; i < input.length(); i+=3) {
-            System.out.print("|");
-            System.out.print(" ");
+//         for(int i = 0; i < input.length(); i+=3) {
+//             System.out.print("|");
+//             System.out.print(" ");
 
-           for(int j = i; (j < i + 3); j++) {
-               System.out.print(input.charAt(j));
-               System.out.print(" ");
-           }
+//            for(int j = i; (j < i + 3); j++) {
+//                System.out.print(input.charAt(j));
+//                System.out.print(" ");
+//            }
 
-           System.out.print("|");
-           System.out.print("\n");
-        } 
+//            System.out.print("|");
+//            System.out.print("\n");
+//         } 
 
-        System.out.println("---------");
+//         System.out.println("---------");
         
+//     }
+// }
+
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int input = scanner.nextInt();
+        System.out.println(input < 10 );
     }
 }
