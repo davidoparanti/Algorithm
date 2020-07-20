96984ce51e46ef50649c05e0dae035321a2da6cc
David Oparanti <david.oparanti@gmail.com>
2020-07-20 15:01:38 +0100

    Add logest class

---
+++ Java/Longest.java
@@ -0,0 +1,27 @@
+
+// Write a program that reads an array of ints and outputs the length of the longest sequence in strictly ascending order. Elements of the sequence must go one after another. A single number is assumed to be ordered sequence with the length = 1.
+
+// Input data format
+
+// The first line contains the size of an array.
+// The second line contains elements of the array separated by spaces.
+
+// Example
+
+// The input array is 1 2 4 1 2 3 5 7 4 3. In this case, the length of the longest sequence in ascending order is 5. It includes the following elements: 1 2 3 5 7.
+
+// Sample Input 1:
+
+// 10
+// 1 2 4 1 2 3 5 7 4 3
+
+// Sample Output 1:
+
+// 5
+import java.util.*;
+class Longest {
+
+    public static void main (String[] args) {
+        
+    }
+}
