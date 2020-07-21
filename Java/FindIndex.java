186bd972a6cb3275c7856f85b3317357579b24aa
David Oparanti <david.oparanti@gmail.com>
2020-07-21 11:56:23 +0100

    Implement FindIndex

--- Java/FindIndex.java
+++ Java/FindIndex.java
@@ -7,6 +7,8 @@ class FindIndex {
         int n = sc.nextInt();
         System.out.println("Enter m");
         int m = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int[] maxIndex = new int[2];
 
         int[][] twoDarray = new int[n][m];
         for (int i = 0; i < twoDarray.length; i++) {
@@ -14,11 +16,20 @@ class FindIndex {
             for(int j = 0; j < twoDarray[i].length; j++){
                 System.out.println("Fill in your m");
                 twoDarray[i][j] = sc.nextInt();
+                if(max < twoDarray[i][j]) {
+                    max = twoDarray[i][j];
+                    maxIndex[0] = i;
+                    maxIndex[1] = j;
+                }
+               // max = max > twoDarray[i][j] ? max : twoDarray[i][j];
             }
         }
-        for(int i = 0; i < twoDarray.length; i++){
-              System.out.println(Arrays.toString(twoDarray[i]));
+        System.out.println();
+        for(int i = 0; i < maxIndex.length; i++){
+              System.out.print(maxIndex[i] + " ");
         }
+        System.out.println();
+      
       
     }
 }
