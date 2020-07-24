67ed0aef3ac401ea42587a0023e2d93bc122afec
David Oparanti <david.oparanti@gmail.com>
2020-07-24 09:11:24 +0100

    Add hello class

---
+++ Java/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        boolean 
+        byte by = 23;
+        short sh = 2134;
+        int in = 202020;
+        long lg = 50000 + 10 * (by + sh + in);
+
+        System.out.println(lg);
+
+    }
+}
