b95379383de1cd6d19a9f360a5e6d97a0bafac3f
David Oparanti <david.oparanti@gmail.com>
2020-07-25 06:29:07 +0100

    Implement hackerrankdaythree

--- Java/HackerrankThree.java
+++ Java/HackerrankThree.java
@@ -41,7 +41,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class HackerrankThree {
 
 
 
@@ -50,6 +50,24 @@ public class Solution {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if(N >= 1 && N <= 100) {
+
+            if(N % 2 != 0) {
+                System.out.println("Weird");
+
+            } else {
+
+                if(N > 20) {
+                    System.out.println("Not Weird");
+                }if(N >= 6 && N <= 20) {
+                    System.out.println("Weird");
+                } if(N >= 1 && N <= 5) {
+                    System.out.println("Not Weird");
+               
+                }
+            }
+        }
 
         scanner.close();
     }
