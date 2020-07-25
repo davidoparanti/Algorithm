a3c0ece2fc2eed13fa4473696a8990ef92827795
David Oparanti <david.oparanti@gmail.com>
2020-07-25 05:27:53 +0100

    Add HackerrankThree quetion

---
+++ Java/HackerrankThree.java
@@ -0,0 +1,56 @@
+/*Task
+Given an integer,
+
+, perform the following conditional actions:
+
+    If 
+
+is odd, print Weird
+If
+is even and in the inclusive range of to
+, print Not Weird
+If
+is even and in the inclusive range of to
+, print Weird
+If
+is even and greater than
+
+    , print Not Weird
+
+Complete the stub code provided in your editor to print whether or not
+
+is weird.
+
+Input Format
+
+A single line containing a positive integer,
+
+.
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
